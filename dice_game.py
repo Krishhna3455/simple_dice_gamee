29302ed204078bda1c775c0c4d8afa56d8e6674a
omlok <olokhande56@gmail.com>
2021-05-18 11:37:23 +0530

    This is a code for simple dice simulator

--- dice_game.py
+++ dice_game.py
@@ -1,1 +1,18 @@
-print("hii")
+import random
+print("Dice is spining.....")
+dice = random.randint(1,6)
+if dice == 6:
+    print(dice,"congrat's!")
+elif dice == 5:
+    print(dice,"exellent!")
+elif dice == 4:
+    print(dice,"fabulous!")
+elif dice == 3:
+    print(dice,"good!")
+elif dice == 2:
+    print(dice,"better!")
+elif dice == 1:
+    print(dice,"better luck next time!")
+else:
+    print()
+
